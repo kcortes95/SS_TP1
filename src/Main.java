a2e644e2dc857f4808ddebe57b33d506c0e6037b
kcortes95 <kcortes95@gmail.com>
2016-03-18 17:20:18 -0300

    Borre lo viejo, cambie el Main y ahora tira otros errores

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Set<Particle> particles = new HashSet<Particle>();
-		Input.readParticles("Dynamic100.txt", "Static100.txt", particles);
+		InputNuevo.readParticles("Dynamic100.txt", "Static100.txt", particles);
 		
 		Grid grid = new CircularGrid(Input.getL(),10,particles);
 		
