c7cc9b14187221328f7dd2c3e31bfac69e94afb7
kcortes95 <kcortes95@gmail.com>
2016-03-22 18:26:48 -0300

    Ya estaria terminado el punto 2. Solo habria que escribirlo en un power(de eso me encargo ahora). Falta el punto 3 que no lo entiendo

--- src/Main.java
+++ src/Main.java
@@ -5,57 +5,74 @@ public class Main {
 	public static void main(String[] args) {
 		Set<Particle> particles = new HashSet<Particle>();
 		
-		System.out.println("Reading files...");
-		Input.readParticles("Dynamic100.txt", "Static100.txt", particles);
-		
-		for(int i=0; i<3000; i++){
-			particles.add(new Particle(0.37,1,Math.random()*100,Math.random()*100));
-		}
-		
-		System.out.println("Starting timer...");
-		long valStart = System.currentTimeMillis();
-		/*
-		System.out.println("Creating grid...");
-		//public CircularGrid(double L, int M, Set<Particle> particles)
-		Grid grid = new CircularGrid(Input.getL(),10,particles);
-		
-		System.out.println("Start simulation");
-		//aca habria que pedir los datos para empezar la simulacion
-		//public Simulation(Grid grid, double totalTime, double intervals, double Rc)
-		//Lo de totalTime es el tiempo inicial, no?? KUYUM HEEEEEELP!
-		
-		Simulation s = new Simulation(grid, 1, 1, 8);
-		s.run();
+		//public static void generateRandomParticles(Set<Particle> particles, int N, double L)
+		generateRandomParticles(particles, 50, 20);
 
-		long valEnd = System.currentTimeMillis();
-		System.out.println("Stop timer");
-		
-		System.out.println("TOTAL TIME IN MILISECONDS: " + (valEnd-valStart));
-		
-		*/
-		timing(500,particles);
-		
+		//timing(int totalRec, Set<Particle> particles, Double Rc, int L)
+	
+		timing(19,particles,1.0,20);
 	}
 	
 
-	public static void timing(int totalRec, Set<Particle> particles){ //agregar Rc acá... y que despues lo llames en Simulation
+	public static void timing(int totalRec, Set<Particle> particles, Double Rc, int L){ 
 		
 		if(totalRec<=0){
 			throw new RuntimeException("M has to be positive");
 		}
 		
+		if(L*1.0/totalRec<=Rc){
+			
+			throw new RuntimeException("No valid M for Cell Index Method. Remember L/M>Rc");
+		}
+		
+		System.out.println("M - Miliseconds");
+		
 		for(int i=1;i<=totalRec;i++){
 			long valStart = System.currentTimeMillis();
 			
-			Grid grid = new CircularGrid(Input.getL(),i,particles);
-			//lo de simulation esta reeeee hardcodeado... habria que pasar algo por parametro, tipo Rc
-			Simulation s = new Simulation(grid, 1, 1, 8);
+			Grid grid = new CircularGrid(L,i,particles);
+			
+			Simulation s = new Simulation(grid, 1, 1, Rc);
 			s.run();
+			
 			long valEnd = System.currentTimeMillis();
-			System.out.println("TOTAL TIME IN MILISECONDS IN " + i + " : " + (valEnd-valStart));
+			
+			System.out.println(i + " - " + (valEnd-valStart));
 		}
-		
 	}
 	
+	public static void generateRandomParticles(Set<Particle> particles, int N, double L){
+		
+		System.out.println("Generating " + N + " random particles, with L: " + L);
+		
+		for(int i=0; i<N; i++){
+			particles.add(new Particle(Math.random(),1,Math.random()*L,Math.random()*L));
+		}
+	}
+	
+	/*
+	//Esto es implementando los imputs que nos brinda la catedra
+	System.out.println("Reading files...");
+	Input.readParticles("Dynamic100.txt", "Static100.txt", particles);
+
+	System.out.println("Starting timer...");
+	long valStart = System.currentTimeMillis();
+
+	System.out.println("Creating grid...");
+	//public CircularGrid(double L, int M, Set<Particle> particles)
+	Grid grid = new CircularGrid(Input.getL(),10,particles);
+	
+	System.out.println("Start simulation");
+	//public Simulation(Grid grid, double totalTime, double intervals, double Rc)
+	
+	Simulation s = new Simulation(grid, 1, 1, 8);
+	s.run();
+
+	long valEnd = System.currentTimeMillis();
+	System.out.println("Stop timer");
+	
+	System.out.println("TOTAL TIME IN MILISECONDS: " + (valEnd-valStart));
+	
+	*/
 	
 }
