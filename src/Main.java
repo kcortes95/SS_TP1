6961e5e5d0df08849c088b402e507a9100d89480
Nico <nicokuyum@hotmail.com>
2016-03-23 09:00:21 -0300

    El Rc se pasa por argumento

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,42 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		int Rc;
+		if(args.length<1 || (Rc=Integer.parseInt(args[0]))<0)
+			throw new RuntimeException("Must provide Rc");
+		
 		Set<Particle> particles = new HashSet<Particle>();
+		
+		//Esto es implementando los imputs que nos brinda la catedra
+		System.out.println("Reading files...");
+		Input.readParticles("Dynamic100.txt", "Static100.txt", particles);
+
+		System.out.println("Starting timer...");
+		long valStart = System.currentTimeMillis();
+
+		System.out.println("Creating grid...");
+		//public LinearGrid(double L, int M, Set<Particle> particles)
+		Grid grid = new LinearGrid(Input.getL(),Rc,particles);
+		
+		System.out.println("Start simulation");
+		//public Simulation(Grid grid, double totalTime, double intervals, double Rc)
+		
+		Simulation s = new Simulation(grid, 1, 1, Rc);
+		s.run();
+
+		long valEnd = System.currentTimeMillis();
+		System.out.println("Stop timer");
+		
+		System.out.println("TOTAL TIME IN MILISECONDS: " + (valEnd-valStart));
+		/*Set<Particle> particles = new HashSet<Particle>();
 		
 		int L = 20;
 		
 		//public static void generateRandomParticles(Set<Particle> particles, int N, double L)
-		generateRandomParticles(particles, 2000, L);
+		generateRandomParticles(particles, 150, L);
 
 		//timing(int totalRec, Set<Particle> particles, Double Rc, int L)
-		timing(19,particles,1.0,L);
+		timing(20,particles,1.0,L);*/
 		
 	}
 	
@@ -22,7 +49,7 @@ public class Main {
 			throw new RuntimeException("M has to be positive");
 		}
 		
-		if(L*1.0/totalRec<=Rc){
+		if(L*1.0/totalRec<Rc){
 			
 			throw new RuntimeException("No valid M for Cell Index Method. Remember L/M>Rc");
 		}
@@ -30,16 +57,18 @@ public class Main {
 		System.out.println("M - Miliseconds");
 		
 		for(int i=1;i<=totalRec;i++){
-			long valStart = System.currentTimeMillis();
-			
-			Grid grid = new CircularGrid(L,i,particles);
-			
-			Simulation s = new Simulation(grid, 1, 1, Rc);
-			s.run();
-			
-			long valEnd = System.currentTimeMillis();
-			
-			System.out.println(i + " - " + (valEnd-valStart));
+			long total = 0;
+			for(int j=0; j<100; j++){
+				long valStart = System.currentTimeMillis();
+				
+				Grid grid = new CircularGrid(L,i,particles);
+				
+				Simulation s = new Simulation(grid, 1, 1, Rc);
+				s.run();
+				
+				total += System.currentTimeMillis()-valStart;
+			}
+			System.out.println(i + " - " + ((double)total/100));
 		}
 	}
 	
@@ -54,27 +83,7 @@ public class Main {
 	}
 	
 	/*
-	//Esto es implementando los imputs que nos brinda la catedra
-	System.out.println("Reading files...");
-	Input.readParticles("Dynamic100.txt", "Static100.txt", particles);
-
-	System.out.println("Starting timer...");
-	long valStart = System.currentTimeMillis();
-
-	System.out.println("Creating grid...");
-	//public CircularGrid(double L, int M, Set<Particle> particles)
-	Grid grid = new CircularGrid(Input.getL(),10,particles);
 	
-	System.out.println("Start simulation");
-	//public Simulation(Grid grid, double totalTime, double intervals, double Rc)
-	
-	Simulation s = new Simulation(grid, 1, 1, 8);
-	s.run();
-
-	long valEnd = System.currentTimeMillis();
-	System.out.println("Stop timer");
-	
-	System.out.println("TOTAL TIME IN MILISECONDS: " + (valEnd-valStart));
 	
 	*/
 	
